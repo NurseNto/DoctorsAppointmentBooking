c2bbe4f630d8cb4278a1da8133928285efddbf8a
DA MAC 29 <damac29@DS-LEARNERs-MacBook-Air-29.local>
2023-06-07 08:33:56 +0200

    hhh

--- Application/Client/project/Appointment/src/app/models/appointment.ts
+++
@@ -1,9 +0,0 @@
-export interface Appointment {
-    id: number;
-    patient_id: number;
-    doctor_id: number;
-    appointment_date: Date;
-    appointment_time: string;
-    notes?: string;
-    status?: string;
-}
--- Application/Client/project/Appointment/src/app/models/doctor.ts
+++
@@ -1,8 +0,0 @@
-export interface Doctor {
-    id: number;
-    name: string;
-    specialty: string;
-    contact_number?: string;
-    email: string;
-    other_details?: string;
-}
