7a1ce2e861ac63d8cdeef29826383708dc534515
NurseNto <nursentombi0@gmail.com>
2023-09-27 12:30:45 +0200

    Save logged user

--- BookingApp/Client/src/app/_components/login/login.component.ts
+++ BookingApp/Client/src/app/_components/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/_services/Auth.service';
+import { SessionService } from 'src/app/_services/session.service';
 
 @Component({
   selector: 'app-login',
@@ -16,7 +17,7 @@ export class LoginComponent implements OnInit {
   isLoggedIn: boolean = false; // Track login status
   
 
-  constructor(private auth:AuthService, private formB: FormBuilder, private router: Router) { 
+  constructor(private auth:AuthService, private formB: FormBuilder, private router: Router, private session: SessionService) { 
     this.loginForm = this.formB.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
@@ -33,12 +34,7 @@ export class LoginComponent implements OnInit {
      this.auth.login(this.loginForm.value).subscribe(res=>{
       this.router.navigate(['/appoint']);
       console.log("success");
-
-     const user = { email: this.email, rememberMe: this.rememberMe };
-     localStorage.setItem('currentUser', JSON.stringify(user));
-
-     //this.isLoggedIn = true; // Set login status to true
-     this.auth.login2
+      this.session.saveLoggedUser(res);
       
      })
 
